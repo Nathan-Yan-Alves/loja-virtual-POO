a0d33f373338e76be6d5e9be7a198861cffa10da
Nathan Yan <nathan_yan_alves@hotmail.com>
2024-09-08 21:40:36 -0300

    Implemented class Client and updated main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from Client import Client
+
+class Main:
+    pass
+
+c1 = Client("Brenda Barbosa Silva", "(61) 7277-4561")
+
+print(c1)
+print(f"Nome do cliente: {c1.name} \nTelefone: {c1.phone_number}")
