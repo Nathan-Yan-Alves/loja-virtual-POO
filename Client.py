c8a41c65a9fd7e7673ac0eadc4bc03050bc6e564
Nathan Yan <nathan_yan_alves@hotmail.com>
2024-09-20 21:12:30 -0300

    Client and Main Modified

--- Client.py
+++ Client.py
@@ -1,4 +1,11 @@
 class Client:
     def __init__(self, name, phone_number):
-        self.name = name
-        self.phone_number = phone_number
+        self._name = name
+        self._phone_number = phone_number
+
+    def get_name(self):
+        return self._name
+
+    def set_info(self, name, phone_number):
+        self._name = name
+        self._phone_number = phone_number
--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from Account import Account
 class Main:
     pass
 
-c1 = Client("Brenda Barbosa Silva", "(61) 7277-4561")
-account1 = Account(c1.name, 6565, 0)
+## c1 = Client("Brenda Barbosa Silva", "(61) 7277-4561")
+c1 = Client()
+c1.set_info("Brenda Barbosa Silva", "(61) 7277-4561")
+account1 = Account(c1.get_name(), 6565, 0)
+
 
 print(f"{account1.holder}\nNumero: {account1.number}\nSaldo: {account1.balance}")
